427241be139d45f84c8810d158f030189f76225b
11113127 <s11113127@stu.edu.tw>
2015-08-24 15:37:54 +0800

    new function

--- second.php
+++ second.php
@@ -1,16 +1,22 @@
 <?php
-
-$list = array (
-    array('aaa', 'bbb', 'ccc', 'dddd'),
-    array('123', '456', '789'),
-    array('"aaa"', '"bbb"')
+$list = array(
+  array("0800000001","name","","min","L0718","0000-00-00"),
+  array("D000000001","name","","min","L0718","0000-00-00")
 );
 
-$fp = fopen('file.csv', 'w');
-
-foreach ($list as $fields) {
-    fputcsv($fp, $fields);
+$filename = date("Y-m-d") . ".csv";
+$f = fopen('php://memory', 'w'); // 寫入 php://memory
+foreach ($list as $row) {
+  fputcsv($f, $row ,',');
 }
+/**rewind the "file" with the csv lines **/
+fseek($f, 0);
 
-fclose($fp);
+/** modify header to be downloadable csv file **/
+header('Content-Type: application/csv');
+header('Content-Disposition: attachement; filename="' . $filename . '";');
+
+/** Send file to browser for download */
+fpassthru($f);
+
 ?>
